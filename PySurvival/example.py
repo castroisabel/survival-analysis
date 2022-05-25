f36976d1904d72bb78da1dc32d8dec2fbe174400
Isabel Castro <icastrocordeiro@gmail.com>
2022-05-25 11:09:55 +0200

    Update example.py

--- PySurvival/example.py
+++ PySurvival/example.py
@@ -11,12 +11,12 @@ X_train, T_train, E_train, X_test, T_test, E_test = \
 # Building a CoxPH model
 coxph_model = CoxPHModel()
 coxph_model.fit(X=X_train, T=T_train, E=E_train, init_method='he_uniform',
-                l2_reg = 1e-4, lr = .4, tol = 1e-4)
+                l2_reg=1e-4, lr=.4, tol=1e-4)
 
 # Building a MTLR model
 mtlr = LinearMultiTaskModel()
 mtlr.fit(X=X_train, T=T_train, E=E_train, init_method = 'glorot_uniform',
-         optimizer ='adam', lr = 8e-4)
+         optimizer='adam', lr=8e-4)
 
 # Checking the model performance
 c_index1 = concordance_index(model=coxph_model, X=X_test, T=T_test, E=E_test )
